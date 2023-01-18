59494a2a820a577043350d21dfe7112467db9dec
Carlos Souza <carloshrsouza@gmail.com>
2023-01-18 15:07:43 -0500

    Add Tailwind CSS

--- config/dev.exs
+++ config/dev.exs
@@ -26,8 +26,10 @@ config :wind_tail, WindTailWeb.Endpoint,
   secret_key_base: "iZd1MT+0SqvMhmhwzyf+A3cWXvIhBTgAtk8gJplorjKI+8nXboo0ktNJdUGtpklU",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
-  ]
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
+  ],
+  server: true
 
 # ## SSL Support
 #
