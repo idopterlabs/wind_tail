897cdb5d96886f5139100bc93b6f4ee549ed2fff
Carlos Souza <carloshrsouza@gmail.com>
2023-03-07 17:35:39 -0500

    Fix page controller tests

--- test/wind_tail_web/controllers/page_controller_test.exs
+++ test/wind_tail_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WindTailWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "WindTail"
   end
 end
