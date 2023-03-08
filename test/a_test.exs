7c150196f14201c208bcaa9a36e7c40373b26409
Carlos Souza <carloshrsouza@gmail.com>
2023-03-07 22:20:41 -0500

    Flaky tests occasionally fail

--- test/a_test.exs
+++ test/a_test.exs
@@ -3,18 +3,33 @@ defmodule WindTail.Test.TestA do
 
   describe "request/1 with valid credentials" do
     test "returns :ok" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 2" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 3" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
+    end
+  end
+
+  describe "request/1 with invalid credentials" do
+    setup do
+      config = Application.get_env(:wind_tail, WindTail.APIClient)
+
+      Application.put_env(:wind_tail, WindTail.APIClient, nil)
+
+      on_exit(fn ->
+        Application.put_env(:wind_tail, WindTail.APIClient, config)
+      end)
+    end
+
+    test "raises error" do
+      assert_raise RuntimeError, fn ->
+        WindTail.APIClient.request("api.foo.com")
+      end
     end
   end
 end
--- test/b_test.exs
+++ test/b_test.exs
@@ -3,17 +3,14 @@ defmodule WindTail.Test.TestB do
 
   describe "request/1 with valid credentials" do
     test "returns :ok" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 2" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 3" do
-      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
   end
