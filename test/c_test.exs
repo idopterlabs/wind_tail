145842e269906b1b3c61fec202a23bf29cce4c98
Carlos Souza <carloshrsouza@gmail.com>
2023-03-07 22:16:05 -0500

    Tests pass faster

--- test/c_test.exs
+++ test/c_test.exs
@@ -1,16 +1,19 @@
 defmodule WindTail.Test.TestC do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   describe "request/1 with valid credentials" do
     test "returns :ok" do
+      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 2" do
+      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
 
     test "returns :ok 3" do
+      Process.sleep(200)
       assert :ok == WindTail.APIClient.request("api.foo.com")
     end
   end
